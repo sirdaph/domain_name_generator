da60d4aba8d2789edbce2c771d37567a2144686e
Daphness Fils-aime <sirdaph@gmail.com>
2019-01-31 00:07:52 +0000

    first commit

---
+++ file.js
@@ -0,0 +1,28 @@
+
+let pronoun =["the", "our"
+    ];
+    
+let adj =["big", "small"
+    ];
+    
+let noun =["shop", "house"
+    ];
+    
+var x;
+var y;
+var z;
+
+   for(x in pronoun){
+       for(y in adj){
+          for( z in noun) { console.log(pronoun[x] + adj[y] +noun[z]+".com");
+                        
+          
+          
+       }
+      
+           
+       }   
+       
+  }
+      
+      
